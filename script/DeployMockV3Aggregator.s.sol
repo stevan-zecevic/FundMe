a6d72daecd9d3924310f48955401eea6c9a57333
stevan-zecevic <stevanzecevic3@gmail.com>
2024-09-14 11:01:39 +0200

    add script to deploy MockV3Aggregator

---
+++ script/DeployMockV3Aggregator.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "test/mock/MockV3Aggregator.t.sol";
+import {Constants} from "script/NetworkConfig.s.sol";
+
+contract DeployMockV3Aggregator is Script, Constants {
+    function run() external returns (MockV3Aggregator) {
+        vm.startBroadcast();
+        MockV3Aggregator aggregator = new MockV3Aggregator(
+            PRICEFEED_DECIMALS,
+            PRICEFEED_INITIAL_ANSWER
+        );
+        vm.stopBroadcast();
+
+        return aggregator;
+    }
+}
